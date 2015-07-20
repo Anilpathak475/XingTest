7cc0fd5403bf8904208a16b5f580d2f3142f541f
jeff Sutton <indieboy1979@gmail.com>
2015-07-20 09:22:05 +0100

    Initial commit. Project structure created

---
+++ xingchallenge/app/src/main/java/uk/me/jeffsutton/xingchallenge/fragment/MainActivityFragment.java
@@ -0,0 +1,13 @@
+package uk.me.jeffsutton.xingchallenge.fragment;
+
+import android.app.ListFragment;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends ListFragment {
+
+    public MainActivityFragment() {
+    }
+
+}
